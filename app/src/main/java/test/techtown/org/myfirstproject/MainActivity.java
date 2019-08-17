f6490194ace3eeeaef03bdbc67a563d389a1fe59
Jihun Cha <jihun5000@naver.com>
2019-08-17 19:54:28 +0900

    [메인 화면 작성]

--- app/src/main/java/test/techtown/org/myfirstproject/MainActivity.java
+++ app/src/main/java/test/techtown/org/myfirstproject/MainActivity.java
@@ -2,8 +2,12 @@ package test.techtown.org.myfirstproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG = MainActivity.class.getSimpleName();
+
+    private Button startApp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
